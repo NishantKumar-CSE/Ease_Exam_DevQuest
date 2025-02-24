bfd10b2e57011ea612ffd03b726ae6a23a00878f
NishantKumar-CSE <nishantkumar.cse8@gmail.com>
2025-02-24 16:37:58 +0530

    Delete my-quiz-app directory

--- my-quiz-app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ExamEase from '../components/ExamEase';
-
-export default function Home() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <ExamEase />
-    </div>
-  );
-}
